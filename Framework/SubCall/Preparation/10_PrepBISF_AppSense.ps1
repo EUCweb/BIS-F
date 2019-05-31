ef2c05970961b73a4ccd50b560e6588484435e09
Matthias Schlimm <info@eucweb.com>
2019-05-31 16:23:47 +0200

    update develop Release with latest BB_1905_branch

--- Framework/SubCall/Preparation/10_PrepBISF_AppSense.ps1
+++ Framework/SubCall/Preparation/10_PrepBISF_AppSense.ps1
@@ -1,23 +1,21 @@
 ﻿<#
-    .Synopsis
-     Prepare AppSense Agent for Image Management
-    .Description
-      lookup for the AppSense Client Communications Agent and prepare the Agent for Imaging
-    .EXAMPLE
-    .Inputs
-    .Outputs
-    .NOTES
-      Author: Matthias Schlimm
-      Editor: Matthias Schhlimm
-      Company: Login Consultants Germany GmbH
+	.SYNOPSIS
+		Prepare AppSense Agent for Image Management
+	.DESCRIPTION
+		Lookup for the AppSense Client Communications Agent and prepare the agent for imaging
+	.EXAMPLE
+	.NOTES
+		Author: Matthias Schlimm
+		Company: Login Consultants Germany GmbH
 
-      History
-      Last Change: 22.03.2016 MS: Script created
-	  Last Change: 28.06.2017 MS: Bugfix 186 - AppSense Product Path - thx to Matthias Kowalkowski
-	  .Link
-    #>
+		History
+		22.03.2016 MS: Script created
+		28.06.2017 MS: Bugfix 186 - AppSense Product Path - thx to Matthias Kowalkowski
+	.LINK
+		https://eucweb.com
+#>
 
-Begin{
+Begin {
 	$script_path = $MyInvocation.MyCommand.Path
 	$script_dir = Split-Path -Parent $script_path
 	$script_name = [System.IO.Path]::GetFileName($script_path)
@@ -29,20 +27,18 @@ Begin{
 
 Process {
 	$svc = Test-BISFService -ServiceName "$servicename" -ProductName "$product"
-	IF ($svc -eq $true)
-	{
+	IF ($svc -eq $true) {
 		Write-BISFLog -Msg "Preperaring $Product for Imaging" -ShowConsole -Color DarkCyan -SubMsg
-		IF (Test-Path ("$product_path\$PrepApp") -PathType Leaf )
-		{
+		IF (Test-Path ("$product_path\$PrepApp") -PathType Leaf ) {
 			Write-BISFLog -Msg "Preparing $Product for Imaging "
 			& Start-Process -FilePath "$product_path\$PrepApp" -ArgumentList "/imageprep" -Wait
-		} ELSE {
+		}
+		ELSE {
 			Write-BISFLog -Msg "$product_path\$PrepApp not exists. Image Preparation could not be performed !!!" -Type E -SubMsg
 		}
 
 	}
 }
-
 
 End {
 	Add-BISFFinishLine
--- Framework/SubCall/Preparation/10_PrepBISF_FSLogix.ps1
+++ Framework/SubCall/Preparation/10_PrepBISF_FSLogix.ps1
@@ -1,34 +1,23 @@
 ﻿<#
-.SYNOPSIS
-  prepare FSLogix Apps for Image Management
+	.SYNOPSIS
+		Prepare FSLogix Apps for Image Management
+	.DESCRIPTION
+		If the preperation ist starting, the script detects the installationn of FsLogix installation and ask to purge the Fslogix Rules on the Master Image
+	.EXAMPLE
+	.NOTES
+		Author: Matthias Schlimm
+		Company: Login Consultants Germany GmbH
 
-.DESCRIPTION
-  If the preperation ist starting, the script detects the installationn of FsLogix installation and ask to purge the Fslogix Rules on the Master Image 
-
-.PARAMETER <Parameter_Name>
-    <Brief description of parameter input required. Repeat this attribute if required>
-
-.INPUTS
-  <Inputs if any, otherwise state None>
-
-.OUTPUTS
-  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
-
-.NOTES
-  Version:        1.0
-  Author:         Matthias Schlimm
-  Creation Date:  03.06.2015
-  Purpose/Change: 03.06.2015 MS: Initial script development
-  Purpose/Change: 13.08.2015 MS: central rules share would be defined and stored in registry location to use it at computer startup
-  Purpose/Change: 21.08.2015 MS: remove to set fsLogix service to manual, stopped service only.
-  Purpose/Change: 30.09.2015 MS: rewritten script with standard .SYNOPSIS, use central BISF function to configure service
-  Purpose/Change: 06.03.2017 MS: Bugfix read Variable $varCLI = ...
-  Purpose/Change: 15.02.2017 MS: Bugfix 237: When in the GPO specify "Configure FSLogix central rule share" to Disabled, the script still prompt for the path when is executed.
-
-  
-  
-.EXAMPLE
- 
+		History:
+		03.06.2015 MS: Initial script development
+		13.08.2015 MS: Central rules share would be defined and stored in registry location to use it at computer startup
+		21.08.2015 MS: Remove to set fsLogix service to manual, stopped service only.
+		30.09.2015 MS: Rewritten script with standard .SYNOPSIS, use central BISF function to configure service
+		06.03.2017 MS: Bugfix read Variable $varCLI = ...
+		15.02.2017 MS: Bugfix 237: When in the GPO specify "Configure FSLogix central rule share" to Disabled, the script still prompt for the path when is executed
+		15.05.2019 JP: Fixed format and deleted junk lines
+	.LINK
+		https://eucweb.com
 #>
 
 Begin {
@@ -42,59 +31,56 @@ Begin {
 }
 
 Process {
-	function ClearConfig
-    {
+	function ClearConfig {
 		Write-BISFLog -Msg "Check Silentswitch..."
-	    $varCLIFS = $LIC_BISF_CLI_FS
-	    IF (($varCLIFS -eq "YES") -or ($varCLIFS -eq "NO")) 
-	    {
-		    Write-BISFLog -Msg "Silentswitch would be set to $varCLIFS"
-	    } ELSE {
-       	    Write-BISFLog -Msg "Silentswitch not defined, show MessageBox"
-		    $MPFS = Show-BISFMessageBox -Msg "Would you like to purge the FsLogix Rules folder ? Note: You must copied the $Product Rules via GPP or your prefered method to your cloned devices !" -Title "$Product" -YesNo -Question
-    	    Write-BISFLog -Msg "$MPFS was selected [YES = purge FsLogix Rules] [NO = Skip rules deletion]"
-	    }
-        
-		if (($MPFS -eq "YES" ) -or ($varCLIFS -eq "YES"))
-	    {
-		    Write-BISFLog -Msg "delete $product Rules" -ShowConsole -Color DarkCyan -SubMsg
-		    Remove-Item -Path "$product_path\Rules\*" -Recurse
-	    } ELSE {
-		    Write-BISFLog -Msg "Skipping $product Rules deletion"
-	    }
+		$varCLIFS = $LIC_BISF_CLI_FS
+		IF (($varCLIFS -eq "YES") -or ($varCLIFS -eq "NO")) {
+			Write-BISFLog -Msg "Silentswitch would be set to $varCLIFS"
+		}
+		ELSE {
+			Write-BISFLog -Msg "Silentswitch not defined, show MessageBox"
+			$MPFS = Show-BISFMessageBox -Msg "Would you like to purge the FsLogix Rules folder ? Note: You must copied the $Product Rules via GPP or your prefered method to your cloned devices !" -Title "$Product" -YesNo -Question
+			Write-BISFLog -Msg "$MPFS was selected [YES = purge FsLogix Rules] [NO = Skip rules deletion]"
+		}
+
+		if (($MPFS -eq "YES" ) -or ($varCLIFS -eq "YES")) {
+			Write-BISFLog -Msg "delete $product Rules" -ShowConsole -Color DarkCyan -SubMsg
+			Remove-Item -Path "$product_path\Rules\*" -Recurse
+		}
+		ELSE {
+			Write-BISFLog -Msg "Skipping $product Rules deletion"
+		}
+	}
+	
+	function Set-RulesShare {
+		# Set the fsLogix central rules share in the BIS-F registry location, to get on BIS-F personalisation on each device
+		Write-BISFLog -Msg "Check Silentswitch..."
+		$varCLIRS = $LIC_BISF_CLI_RSb
+		$varCLIRS = $LIC_BISF_CLI_RSb
+		$varCLIRS = $LIC_BISF_CLI_RSb
+		IF ($varCLIRS -ne "") {
+			Write-BISFLog -Msg "Silentswitch would be set to $varCLIRS"
+			$fslogixRulesShare = $LIC_BISF_CLI_RS
+		}
+		ELSE {
+			Write-BISFLog -Msg "Silentswitch not defined, show Inputprompt to define UNC-Path, where the fslogix Rules (frx) and Assignments (fxa) would be stored"
+			$MPRS = Show-BISFCustomInputBox -title "Fslogix Central Rules Share" -message "Please enter a central rules share, where do you stored the fsLogix Rules (frx) and Assignment (fxa) files. Enter a valid UNC-Path, that be accessible at computer startup with system account" ""
+			$fslogixRulesShare = $MPRS
+		}
+
+		if ($fslogixRulesShare -ne "") {
+			Write-BISFLog -Msg "The fsLogix Central Rules Share would be set to $fslogixRulesShare" -ShowConsole -Color DarkCyan -SubMsg
+			Write-BISFLog -Msg "set fsLogix Central Rules Share in the registry $hklm_software_LIC_CTX_BISF_SCRIPTS, Name LIC_BISF_FSXRulesShare, value $fslogixRulesShare"
+			Set-ItemProperty -Path $hklm_software_LIC_CTX_BISF_SCRIPTS -Name "LIC_BISF_FSXRulesShare" -value "$fslogixRulesShare" -Force
+
+		}
+		ELSE {
+			Write-BISFLog -Msg "No fsLogix Central Rules Share defined"
+		}
 	}
 
-    function Set-RulesShare
-    {
-        #set the fsLogix central rules share in the BIS-F registry location, to get on BIS-F personalisation on each device
-        Write-BISFLog -Msg "Check Silentswitch..."
-	    $varCLIRS = $LIC_BISF_CLI_RSb
-	    IF ($varCLIRS -ne "")
-	    {
-		    Write-BISFLog -Msg "Silentswitch would be set to $varCLIRS"
-            $fslogixRulesShare = $LIC_BISF_CLI_RS
-	    } ELSE {
-       	    Write-BISFLog -Msg "Silentswitch not defined, show Inputprompt to define UNC-Path, where the fslogix Rules (frx) and Assignments (fxa) would be stored"
-            $MPRS = Show-BISFCustomInputBox -title "Fslogix Central Rules Share" -message "Please enter a central rules share, where do you stored the fsLogix Rules (frx) and Assignment (fxa) files. Enter a valid UNC-Path, that be accessible at computer startup with system account" ""
-            $fslogixRulesShare = $MPRS
-        }
-
-        if ($fslogixRulesShare -ne "") 
-	    {
-		    Write-BISFLog -Msg "The fsLogix Central Rules Share would be set to $fslogixRulesShare" -ShowConsole -Color DarkCyan -SubMsg
-            Write-BISFLog -Msg "set fsLogix Central Rules Share in the registry $hklm_software_LIC_CTX_BISF_SCRIPTS, Name LIC_BISF_FSXRulesShare, value $fslogixRulesShare"
-            Set-ItemProperty -Path $hklm_software_LIC_CTX_BISF_SCRIPTS -Name "LIC_BISF_FSXRulesShare" -value "$fslogixRulesShare" -Force
-		    
-	    } ELSE {
-		    Write-BISFLog -Msg "No fsLogix Central Rules Share defined"
-	    }
-    }
-
-
-
 	$svc = Test-BISFService -ServiceName "$servicename" -ProductName "$product"
-	IF ($svc -eq $true)
-	{
+	IF ($svc -eq $true) {
 		Invoke-BISFService -ServiceName "$servicename" -Action Stop
 		ClearConfig
 		Set-RulesShare
