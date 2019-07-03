b4a4d7b17913c8856976d28fce995a3ced4f1f6e
Matthias Schlimm <34108484+matthias-schlimm@users.noreply.github.com>
2019-07-03 18:58:34 +0200

    ENH 117: get logfile of the DISM Process into BIS-F log

--- Framework/SubCall/Global/BISF.psm1
+++ Framework/SubCall/Global/BISF.psm1
@@ -1549,7 +1549,7 @@ function Optimize-WinSxs {
       	Last Change: 07.01.2016 MS: function created
 		Last Change: 17.05.2019 MS: HF 106: remove uneccesary out-null command function Optimize-WinSxs
 		Last Change: 21.06.2019 MS: FRQ 115: ADMX: Control of WinSxS Optimization
-		Last Change: 27.06.2019 MS: ENH 117: WinSxS hide DISM process and get logfile
+		Last Change: 03.07.2019 MS: ENH 117: WinSxS hide DISM process and get logfile of the DISM Process into BIS-F log
 	.Link
 #>
     Write-BISFFunctionName2Log -FunctionName ($MyInvocation.MyCommand | % {$_.Name})  #must be added at the begin to each function
@@ -1570,10 +1570,10 @@ function Optimize-WinSxs {
 
 		}
 		IF ($runWinSxs -eq 1) {
-			IF (!($LIC_BISF_CLI_WinSxSTimeout)) {$LIC_BISF_CLI_WinSxSTimeout = 60}
-			Start-Process 'Dism.exe' -ArgumentList '/online /Cleanup-Image /StartComponentCleanup /ResetBase' -RedirectStandardOutput "C:\windows\temp\WinSxs.log" -NoNewWindow
+			IF (Test-path "C:\Windows\logs\DISM\dism_bisf.log") {Remove-Item "C:\Windows\logs\DISM\dism_bisf.log" -Force}
+                	Start-Process 'Dism.exe' -ArgumentList '/online /Cleanup-Image /StartComponentCleanup /ResetBase /Logpath:C:\Windows\logs\DISM\dism_bisf.log' -RedirectStandardOutput "C:\windows\temp\WinSxs.log" -NoNewWindow
 			Show-BISFProgressBar -CheckProcess "Dism" -ActivityText "run DISM to cleanup WinSxs Folder ...(max. Execution Timeout $LIC_BISF_CLI_WinSxSTimeout min)" -MaximumExecutionMinutes $LIC_BISF_CLI_WinSxSTimeout
-		    	Get-BISFLogContent -GetLogFile "C:\windows\temp\WinSxs.log"
+		        Get-BISFLogContent -GetLogFile "C:\Windows\logs\DISM\dism_bisf.log"
 		} ELSE {
 			Write-BISFLog -Msg "WinSxS Optimization will not run (runWinSxs = 0)" -ShowConsole -SubMsg -Color DarkCyan
 		}
