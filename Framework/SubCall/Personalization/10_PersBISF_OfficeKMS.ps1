52933ed519dc529c3adba4e989a4989482dcff91
Matthias Schlimm <matthias.schlimm@online.de>
2020-01-07 17:02:17 +0100

    HF 174 - Office detection general change

--- Framework/SubCall/Personalization/10_PersBISF_OfficeKMS.ps1
+++ Framework/SubCall/Personalization/10_PersBISF_OfficeKMS.ps1
@@ -15,6 +15,7 @@
 		22.03.2018 MS: Feature 15 - support for Office 365 ClicktoRun
 		28.03.2019 MS: FRQ 86 - Office 2019 support
 		03.10.2019 MS: ENH 84 - if hosting on azure, Displays the device join status
+		07.01.2020 MS: HF 174 - Office detection general change
 	.LINK
         https://eucweb.com
 #>
@@ -28,102 +29,49 @@ Begin {
 
 Process {
 
-	# Check the installation path of Office 2010
-	$Office2010InstallRoot = $null
-	If ([Environment]::Is64BitOperatingSystem) {
-		$Office2010InstallRoot = (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Wow6432Node\Microsoft\Office\14.0\Common\InstallRoot -Name Path -ErrorAction SilentlyContinue).Path
-	}
-	If ($Office2010InstallRoot -isnot [system.object]) { $Office2010InstallRoot = (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Microsoft\Office\14.0\Common\InstallRoot -Name Path -ErrorAction SilentlyContinue).Path }
+	# Check Office installation
+	$OfficeInstallations = Get-WmiObject win32_product | where { $_.Name -like "Microsoft Office Professional Plus*" -or $_.Name -Like "Microsoft Office Standard*" -or $_.Name -like "*Click-to-Run Licensing Component*" }
+	[array]$OfficeInstallRoot = $null
 
-	# Check the installation path of Office 2013
-	$Office2013InstallRoot = $null
-	If ([Environment]::Is64BitOperatingSystem) {
-		$Office2013InstallRoot = (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Wow6432Node\Microsoft\Office\15.0\Common\InstallRoot -Name Path -ErrorAction SilentlyContinue).Path
-	}
-	If ($Office2013InstallRoot -isnot [system.object]) { $Office2013InstallRoot = (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Microsoft\Office\15.0\Common\InstallRoot -Name Path -ErrorAction SilentlyContinue).Path }
-
-	# Check the installation path of Office 2016
-	$Office2016InstallRoot = $null
-	If ([Environment]::Is64BitOperatingSystem) {
-		$Office2016InstallRoot = (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Wow6432Node\Microsoft\Office\16.0\Common\InstallRoot -Name Path -ErrorAction SilentlyContinue).Path
-	}
-	If ($Office2016InstallRoot -isnot [system.object]) { $Office2016InstallRoot = (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Microsoft\Office\16.0\Common\InstallRoot -Name Path -ErrorAction SilentlyContinue).Path }
-
-	# Check the installation path of Office 2019
-	$Office2019InstallRoot = $null
-	If ([Environment]::Is64BitOperatingSystem) {
-		$Office2019InstallRoot = (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Wow6432Node\Microsoft\Office\17.0\Common\InstallRoot -Name Path -ErrorAction SilentlyContinue).Path
-	}
-	If ($Office2019InstallRoot -isnot [system.object]) { $Office2019InstallRoot = (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Microsoft\Office\17.0\Common\InstallRoot -Name Path -ErrorAction SilentlyContinue).Path }
-
-
-	# Check the installation path of Office 365 ClickToRun
-	$Office365InstallRoot = $null
-	If ([Environment]::Is64BitOperatingSystem) {
-		$Office365InstallRoot = (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Wow6432Node\Microsoft\Office\ClickToRun -Name InstallPath -ErrorAction SilentlyContinue).Path
-	}
-	If ($Office365InstallRoot -isnot [system.object]) { $Office365InstallRoot = (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Microsoft\Office\ClickToRun -Name InstallPath -ErrorAction SilentlyContinue).Path }
-
-
-
-	# Activate the office version if installed
-	$result = $null
-	IF ($Office2010InstallRoot -is [System.Object]) {
-		Write-BISFLog -msg "Office 2010 is installed" -ShowConsole -Color Cyan
-		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$($Office2010InstallRoot)OSPP.VBS"" /act" -ActText "Start triggering activation"
-		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$($Office2010InstallRoot)OSPP.VBS"" /dstatus" -ActText "Get Office Licensing state"
-	}
- ELSE {
-		Write-BISFLog -msg "Office 2010 is NOT installed"
-	}
-
-	IF ($Office2013InstallRoot -is [System.Object]) {
-		Write-BISFLog -msg "Office 2013 is installed" -ShowConsole -Color Cyan
-		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$($Office2013InstallRoot)OSPP.VBS"" /act" -ActText "Start triggering activation"
-		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$($Office2013InstallRoot)OSPP.VBS"" /dstatus" -ActText "Get Office Licensing state"
-	}
- ELSE {
-		Write-BISFLog -msg "Office 2013 is NOT installed"
-	}
-
-
-	IF ($Office2016InstallRoot -is [System.Object]) {
-		Write-BISFLog -msg "Office 2016 is installed" -ShowConsole -Color Cyan
-		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$($Office2016InstallRoot)OSPP.VBS"" /act" -ActText "Start triggering activation"
-		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$($Office2016InstallRoot)OSPP.VBS"" /dstatus" -ActText "Get Office Licensing state"
-	}
- ELSE {
-		Write-BISFLog -msg "Office 2016 is NOT installed"
-	}
-
-	IF ($Office2019InstallRoot -is [System.Object]) {
-		Write-BISFLog -msg "Office 2019 is installed" -ShowConsole -Color Cyan
-		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$($Office2019InstallRoot)OSPP.VBS"" /act" -ActText "Start triggering activation"
-		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$($Office2019InstallRoot)OSPP.VBS"" /dstatus" -ActText "Get Office Licensing state"
-	}
- ELSE {
-		Write-BISFLog -msg "Office 2019 is NOT installed"
-	}
-
-	IF ($Office365InstallRoot -is [System.Object]) {
-		Write-BISFLog -msg "Office 365 is installed" -ShowConsole -Color Cyan
-		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$($Office365InstallRoot)OSPP.VBS"" /act" -ActText "Start triggering activation"
-		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$($Office365InstallRoot)OSPP.VBS"" /dstatus" -ActText "Get Office Licensing state"
-
-		$O365onAzure = Test-BISFAzureVM
-		IF ($O365onAzure -eq $true) {
-			Write-BISFLog -Msg "Office 365 is hosting on Microsoft Azure" -ShowConsole -Color DarkCyan -SubMsg
-			Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\dsregcmd.exe" -Args "/status" -ActText "Office - Displays the device join status"
+	ForEach ($Office in $OfficeInstallations) {
+		$OFName = $Office.Name
+		$OFVersion = $Office.Version						#Version : 16.0.4266.1001
+		$OFVersionShort = $OFVersion.substring(0, 4)  	#Version : 16.0
+		IF ($OFName -like "*Click-to-Run*") { $O365 = $true } ELSE { $O365 = $false }
+		Write-BISFLog -Msg "$OFName - $OFVersion installed" -ShowConsole -Color Cyan
+		IF ($O365 -eq $false) {
+			If ([Environment]::Is64BitOperatingSystem) {
+				$OfficeInstallRoot += (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Wow6432Node\Microsoft\Office\$($OFVersionShort)\Common\InstallRoot -Name Path -ErrorAction SilentlyContinue).Path
+			}
+			If ($OfficeInstallRoot -isnot [system.object]) { $OfficeInstallRoot += (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Microsoft\Office\$($OFVersionShort)\Common\InstallRoot -Name Path -ErrorAction SilentlyContinue).Path }
 		}
 		ELSE {
-			Write-BISFLog -Msg "Office 365 is NOT hosting on Microsoft Azure" -Color DarkCyan -SubMsg
+			If ([Environment]::Is64BitOperatingSystem) {
+				$OfficeInstallRoot += (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Wow6432Node\Microsoft\Office\ClickToRun -Name InstallPath -ErrorAction SilentlyContinue).InstallPath
+			}
+			If ($OfficeInstallRoot -isnot [system.object]) { $OfficeInstallRoot += (Get-ItemProperty -Path Registry::HKLM\SOFTWARE\Microsoft\Office\ClickToRun -Name InstallPath -ErrorAction SilentlyContinue).InstallPath }
 		}
-
+		Write-BISFLog -Msg "Installpath $OfficeInstallRoot " -ShowConsole -Color DarkCyan -SubMsg
+		$OSPP = Get-ChildItem -Path $OfficeInstallRoot -filter "OSPP.vbs" -Recurse -ErrorAction SilentlyContinue | ForEach-Object { $_.FullName }
+		Write-BISFLog -Msg "OSPP is installed in $OSPP"
+		# Activate the office version
+		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$OSPP"" /act" -ActText "Start triggering activation"
+		Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\cscript.exe" -Args "//NoLogo ""$OSPP"" /dstatus" -ActText "Get Office Licensing state"
+		IF ($O365 -eq $true) {
+			$O365onAzure = Test-BISFAzureVM
+			IF ($O365onAzure -eq $true) {
+				Write-BISFLog -Msg "Office 365 is hosting on Microsoft Azure" -ShowConsole -Color DarkCyan -SubMsg
+				Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\dsregcmd.exe" -Args "/status" -ActText "Office - Displays the device join status"
+			}
+			ELSE {
+				Write-BISFLog -Msg "Office 365 is NOT hosting on Microsoft Azure" -Color DarkCyan -SubMsg
+			}
+		}
 	}
- ELSE {
-		Write-BISFLog -msg "Office 365 is NOT installed"
-	}
 
+	IF ($null -eq $OfficeInstallRoot ) {
+		Write-BISFLog -Msg "No Office installation detected" -Type W
+	}
 
 }
 
